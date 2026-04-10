ed5aee34928cc94cf72219cb002301dfca9b645c
i2221207-eng <i2221207@continental.edu.pe>
2026-04-10 13:56:47 -0500

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,13 @@
 import { Sequelize } from 'sequelize'
-import dotenv from 'dotenv'
 
-dotenv.config()
-
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+const db = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-)
+})
 
 export default db
