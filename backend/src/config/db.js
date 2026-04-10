39f4067e8b3dd798df0cfd5aa03090da7b5c6d74
i2221207-eng <i2221207@continental.edu.pe>
2026-04-10 14:01:24 -0500

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,18 @@ import { Sequelize } from 'sequelize'
 
 const db = new Sequelize(process.env.MYSQL_URL, {
   dialect: 'mysql',
+  logging: false,
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 })
 
